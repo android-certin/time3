ec90464abc0b2aea7999580cdfb7e3e99ff28bf5
Jean Pimentel <jpimentel@ciandt.com>
2015-08-26 15:36:52 -0300

    Adiciona directions

--- WorldWonders/app/src/main/java/com/ciandt/worldwonders/activities/WonderDetailActivity.java
+++ WorldWonders/app/src/main/java/com/ciandt/worldwonders/activities/WonderDetailActivity.java
@@ -3,6 +3,7 @@ package com.ciandt.worldwonders.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -30,7 +31,7 @@ public class WonderDetailActivity extends AppCompatActivity {
 
     CollapsingToolbarLayout collapsingToolbarLayout;
     Toolbar toolbar;
-
+    Menu menu;
     ImageView image;
     TextView description;
 
@@ -68,14 +69,20 @@ public class WonderDetailActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        this.menu = menu;
         getMenuInflater().inflate(R.menu.menu_wonderdetail, menu);
+
+        MenuItem directionsItem = menu.findItem(R.id.action_directions);
+        if (wonder.latitude == 0.0 && wonder.longitude == 0.0) {
+            directionsItem.setVisible(false);
+        }
 
         Intent share = new Intent(Intent.ACTION_SEND);
         share.setType("text/plain");
         share.putExtra(Intent.EXTRA_TEXT, wonder.description);
 
-        MenuItem menuItem = menu.findItem(R.id.action_share);
-        ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(menuItem);
+        MenuItem shareItem = menu.findItem(R.id.action_share);
+        ShareActionProvider shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareItem);
         shareActionProvider.setShareIntent(share);
 
         return true;
@@ -88,12 +95,41 @@ public class WonderDetailActivity extends AppCompatActivity {
 
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (id) {
+
+            case R.id.action_directions:
+                return startDirections();
+
+            case R.id.action_bookmark:
+                return toggleBookmark();
+
+            default:
+                break;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public boolean startDirections() {
+
+        Uri uri = Uri.parse(String.format("geo:%f,%f?q=%f,%f(%s)", wonder.latitude, wonder.longitude, wonder.latitude, wonder.longitude, Uri.encode(wonder.name)));
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, uri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean toggleBookmark() {
+
+        
+
+
+        return false;
     }
 
 }
